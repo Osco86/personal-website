28cde2b01888255d68b41170119e12e7c41d5659
Oscar <oscar.huyghe09@gmail.com>
2021-08-16 22:27:34 +0200

    Fix build

--- components/Link.js
+++ components/Link.js
@@ -73,6 +73,7 @@ Link.propTypes = {
   prefetch: PropTypes.bool,
 };
 
+// eslint-disable-next-line react/display-name
 export default React.forwardRef((props, ref) => (
   <Link {...props} innerRef={ref} />
 ));
